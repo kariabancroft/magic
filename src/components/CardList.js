32bb204d6eb5e8a90f5752ccfc71ff5777f9d3f6
Kari Bancroft <kariabancroft@gmail.com>
2019-06-25 23:22:26 -0700

    add inifinite scroll integration to card list

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,53 +2,46 @@ import React from 'react';
 import axios from 'axios';
 import './CardList.css'
 import Card from './Card'
+import InfiniteScroll from 'react-infinite-scroller'
 
 class CardList extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      results: [],
-      loading: true,
-      page: 0
+      results: []
     }
   }
 
-  loadCards() {
-    const pageNum = (this.state.page + 1)
-    const apiParams = '?type=creature&orderBy=name&pageSize=20&page=' + pageNum
-    axios.get(process.env.REACT_APP_API_URL + apiParams)
+  loadCards = (page) => {    
+    axios.get(process.env.REACT_APP_API_URL, {
+      params: {
+        type: 'creature',
+        orderBy: 'name',
+        pageSize: 20,
+        page: page
+      }
+    })
       .then((response) => {
-        this.setState({results: response.data.cards, page: pageNum})
+        this.setState({results: response.data.cards})
       })
       .catch((error) => {
         this.setState({error: 'Error has occurred. ' + error})
       })
-      .then(() => {
-        this.setState({loading: false})
-      })
-  }
-
-  componentDidMount() {
-    this.loadCards()
   }
 
   render() {
-    let data;
+    const loader = <img className='loading' alt='loading indicator'
+      src='https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif' />
 
-    if (this.state.loading) {
-      data = <img className='loading' alt='loading indicator'
-        src='https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif' />
-    } else {
-      data = this.state.results.map((card, index) => {
-        return <Card key={index}
-          img={card.imageUrl}
-          artist={card.artist}
-          name={card.name}
-          originalType={card.type}
-          setName={card.setName} />
-      })
-    }
+    const cards = this.state.results.map((card, index) => {
+      return <Card key={index}
+        img={card.imageUrl}
+        artist={card.artist}
+        name={card.name}
+        originalType={card.type}
+        setName={card.setName} />
+    })
 
     let err;
     if (this.state.error) {
@@ -56,10 +49,17 @@ class CardList extends React.Component {
     }
 
     return (
-      <div className='card-list'>
-        {err}
-        {data}
-      </div>
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={this.loadCards}
+        hasMore={true}
+        loader={loader}>
+
+        <div className='card-list'>
+          {err}
+          {cards}
+        </div>
+      </InfiniteScroll>
     )
   }
 }
